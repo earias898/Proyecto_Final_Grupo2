1e61b9564165f378a1bcfaf08f339c9d1cf26835
dcp27 <73448167+dcp27@users.noreply.github.com>
2020-11-28 22:40:33 -0600

    Update clsModuloTurismo.java

--- src/main/java/prjproyectofinalgrupo3/prjproyectofinalgrupo3/clsModuloTurismo.java
+++ src/main/java/prjproyectofinalgrupo3/prjproyectofinalgrupo3/clsModuloTurismo.java
@@ -7,8 +7,70 @@ package prjproyectofinalgrupo3.prjproyectofinalgrupo3;
 
 /**
  *
- * @author esteb
+ * @author DonnkanCervantes
  */
 public class clsModuloTurismo {
-    
+    rivate String direccion, correo, paginaWeb, descripcion;
+   private int telefono, precioNoche;
+
+    public clsModuloTurismo(String direccion, String correo, String paginaWeb, String descripcion, int telefono, int precioNoche) {
+        this.direccion = direccion;
+        this.correo = correo;
+        this.paginaWeb = paginaWeb;
+        this.descripcion = descripcion;
+        this.telefono = telefono;
+        this.precioNoche = precioNoche;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPaginaWeb() {
+        return paginaWeb;
+    }
+
+    public void setPaginaWeb(String paginaWeb) {
+        this.paginaWeb = paginaWeb;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        if (telefono >= 99999999 || telefono < 0) 
+            this.telefono = telefono;
+    }
+
+    public int getPrecioNoche() {
+        return precioNoche;
+    }
+
+    public void setPrecioNoche(int precioNoche) {
+                if(precioNoche > 0)
+        this.precioNoche = precioNoche;
+    }
+   
+   
 }
