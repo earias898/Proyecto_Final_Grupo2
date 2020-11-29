270fc0eeed98743ba0743a6f2bd3b6c37b6f3c8c
dcp27 <73448167+dcp27@users.noreply.github.com>
2020-11-28 22:31:58 -0600

    Update clsModuloTransporte.java

--- src/main/java/prjproyectofinalgrupo3/prjproyectofinalgrupo3/clsModuloTransporte.java
+++ src/main/java/prjproyectofinalgrupo3/prjproyectofinalgrupo3/clsModuloTransporte.java
@@ -7,8 +7,54 @@ package prjproyectofinalgrupo3.prjproyectofinalgrupo3;
 
 /**
  *
- * @author esteb
+ * @author DonnkanCervantes
  */
 public class clsModuloTransporte {
+    private String ruta, pago;
+    private int telefono, tarifa;
     
+    public ModuloTransporte(String ruta, String pago, int telefono, int tarifa) {
+        this.ruta = ruta;
+        this.pago = pago;
+        this.telefono = telefono;
+        this.tarifa = tarifa;
+    }
+    
+    public boolean checkLength(int id, int length) {
+    return Math.log10(id) < length;
+  }
+    
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+
+    public String getPago() {
+        return pago;
+    }
+
+    public void setPago(String pago) {
+        this.pago = pago;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+      (if (telefono >= 99999999 || telefono < 0) 
+        this.telefono = telefono;
+    }
+
+    public int getTarifa() {
+        return tarifa;
+    }
+
+    public void setTarifa(int tarifa) {
+        if(tarifa > 0)
+        this.tarifa = tarifa;
+    }
 }
