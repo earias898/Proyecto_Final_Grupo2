41f7ce2e88585f532aef4900380c95ff6c4c7544
Orakmin <75190001+Orakmin@users.noreply.github.com>
2020-11-28 22:08:57 -0600

    Update clsModuloCultura.java

--- src/main/java/prjproyectofinalgrupo3/prjproyectofinalgrupo3/clsModuloCultura.java
+++ src/main/java/prjproyectofinalgrupo3/prjproyectofinalgrupo3/clsModuloCultura.java
@@ -7,8 +7,75 @@ package prjproyectofinalgrupo3.prjproyectofinalgrupo3;
 
 /**
  *
- * @author esteb
+ * @author orak
  */
 public class clsModuloCultura {
     
+    private int cedulaJuridica;
+    private String tipoDanzas;
+    private String descripcion;
+    private int telefono;
+    private String direccion;
+    private String correo;
+    private String paginaWeb;
+    
+
+
+public clsModuloCultura(){
+    
+ }
+    
+    public clsModuloCultura(int cedulaJuridica, String tipoDanzas, String descripcion, int telefono, String direccion, String correo, String paginaWeb) {
+        this.cedulaJuridica = cedulaJuridica;
+        this.tipoDanzas = tipoDanzas;
+        this.descripcion = descripcion;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.correo = correo;
+        this.paginaWeb = paginaWeb;
+    }
+    public void setCedulaJuridica (int cedulaJuridica){
+        if(cedulaJuridica > 0)
+            this.cedulaJuridica = cedulaJuridica;
+    }
+    public int getCedulaJuridica (){
+        return cedulaJuridica;
+    }
+    public String getTipodeDanzas() {
+        return tipoDanzas;
+    }
+    public void setTipoDeDanzas(String tipoDanzas) {
+        this.tipoDanzas = tipoDanzas;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public float getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    public String getPaginaWeb() {
+        return paginaWeb;
+    }
+    public void setPaginaWeb(String paginaWeb) {
+        this.paginaWeb = paginaWeb;
+    }
+
 }
